ef5a7284368a1fc6a5b6257a83acb8d0dd5f33e5
osama0199 <97133652+osama0199@users.noreply.github.com>
2022-03-14 22:33:44 +0600

    Update Employee.kt

--- src/main/kotlin/Employee/Employee.kt
+++ src/main/kotlin/Employee/Employee.kt
@@ -1,3 +1,12 @@
+/*BaseSalariedEmployee -------
+** double Salary
+HourlySalariedEmployee --------
+**double totalHour
+** double hourlyRate
+CommissionedSalariedEmployee ----------------
+**double Salary
+** double total Salary
+** double commissionRate */
 package Employee
 
 abstract class Employee(val name: String) { //in kotlin, by default every class is declared as final which can't be accessed. use open keyword to make it accessible
