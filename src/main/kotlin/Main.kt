5fd5f475dba82177d1310dc65250a9cd96890531
Rishad Amin Pulok <rishad15-2117@diu.edu.bd>
2022-03-14 11:45:14 +0600

    polymorphism example

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 import Employee.BaseSalariedEmp
 import Employee.CommissionSalariedEmp
+import Employee.Employee
 import Employee.HourlySalariedEmp
 import PencilBox.PaidCourse
 import PencilBox.SEIP
@@ -11,9 +12,14 @@ fun main() {
     val hse = HourlySalariedEmp("Siam",60.0)
     val cse = CommissionSalariedEmp("Pulok",20000.0,50000.0,5.0)
 
-    println("Salary of Base salaried employee: ${bse.calculateSalary()}")
+    val employees: List<Employee> = listOf(bse,hse,cse) //example of runtime polymorphism. Pre-condition of polymorphism -> child classes must override parent class component
+    for(employee in employees){
+        println("Salary of ${employee.name}: ${employee.calculateSalary()}")
+    }
+
+    /*println("Salary of Base salaried employee: ${bse.calculateSalary()}")
     println("Salary of Hourly salaried employee: ${hse.calculateSalary()}")
-    println("Salary of Commission salaried employee: ${cse.calculateSalary()}")
+    println("Salary of Commission salaried employee: ${cse.calculateSalary()}")*/
 
     val seip = SEIP("Mobile Application Development",9000.0,"3 months")
     val paid = PaidCourse("Flutter",5000.0,"3 months")
