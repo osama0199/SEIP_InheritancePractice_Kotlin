8d7fd73a037fb3f816446993817c7d032bfa121e
Rishad Amin Pulok <rishad15-2117@diu.edu.bd>
2022-03-14 11:56:32 +0600

    polymorphism example updated

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,17 +12,26 @@ fun main() {
     val hse = HourlySalariedEmp("Siam",60.0)
     val cse = CommissionSalariedEmp("Pulok",20000.0,50000.0,5.0)
 
-    val employees: List<Employee> = listOf(bse,hse,cse) //example of runtime polymorphism. Pre-condition of polymorphism -> child classes must override parent class component
+    //For Employee package
+    val manager = salaryManager(bse)
+    println("Salary: ${manager.pay()}")
+
+    /*val employees: List<Employee> = listOf(bse,hse,cse) //example of runtime polymorphism. Pre-condition of polymorphism -> child classes must override parent class component
     for(employee in employees){
         println("Salary of ${employee.name}: ${employee.calculateSalary()}")
     }
-
-    /*println("Salary of Base salaried employee: ${bse.calculateSalary()}")
+    println("Salary of Base salaried employee: ${bse.calculateSalary()}")
     println("Salary of Hourly salaried employee: ${hse.calculateSalary()}")
     println("Salary of Commission salaried employee: ${cse.calculateSalary()}")*/
 
+    /*//For Pencilbox package
     val seip = SEIP("Mobile Application Development",9000.0,"3 months")
     val paid = PaidCourse("Flutter",5000.0,"3 months")
     println(seip.getInfo())
-    println(paid.getInfo())
+    println(paid.getInfo())*/
+}
+
+class salaryManager(val employee: Employee){
+    fun pay() = employee.calculateSalary()
+
 }
